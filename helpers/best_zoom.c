fd43445b914135bcbe093cfae6acc4e6fca10ece
Tareq Bareich <tbareich@student.1337.ma>
2019-11-10 06:42:11 +0100

    1337 colored

--- helpers/best_zoom.c
+++ helpers/best_zoom.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   best_zoom.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mesafi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: tbareich <tbareich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/08 17:25:48 by mesafi            #+#    #+#             */
-/*   Updated: 2019/11/08 17:26:00 by mesafi           ###   ########.fr       */
+/*   Updated: 2019/11/10 06:31:15 by tbareich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int		best_zoom(int height, int width)
 	int		h;
 	int		w;
 
-	h = (float)H / 5;
-	w = (float)W / 5;
+	h = (float)H / 3;
+	w = (float)W / 3;
 	return ((h / height) < (w / width) ? (h / height) : (w / width));
 }
